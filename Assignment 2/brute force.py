0893e76519d3b57fd7f6ca136393949db7cf67c0
Mohammad-Sufyan-Azam <sufyan20312@iiitd.ac.in>
2024-03-04 16:42:00 +0530

    Completed code by finding a single best motif

--- Assignment 2/brute force.py
+++ Assignment 2/brute force.py
@@ -4,85 +4,79 @@ import time
 
 
 # Function to calculate the score of a motif
-def get_best_motif(motif):
-    L = len(motif[0])
-    T = len(motif)
-    profile_matrix = np.zeros((4, L))   # 4*L matrix
-    
-    best_motif = ''
+def get_best_motif(profile_matrix):
+    best_motif = ""
     best_score = 0
-    # print(motif)
-
-    for i in range(L):
-        score = 0
-        char = ''
-        
-        for j in range(T):
-            # print(motif[j][i], end='')
-            if motif[j][i].upper() == 'A':
-                profile_matrix[0][i] += 1
-            elif motif[j][i].upper() == 'C':
-                profile_matrix[1][i] += 1
-            elif motif[j][i].upper() == 'G':
-                profile_matrix[2][i] += 1
-            elif motif[j][i].upper() == 'T':
-                profile_matrix[3][i] += 1
-            
-            if profile_matrix[0][i] > score:
-                score = profile_matrix[0][i]
-                char = 'A'
-            elif profile_matrix[1][i] > score:
-                score = profile_matrix[1][i]
-                char = 'C'
-            elif profile_matrix[2][i] > score:
-                score = profile_matrix[2][i]
-                char = 'G'
-            elif profile_matrix[3][i] > score:
-                score = profile_matrix[3][i]
-                char = 'T'
-        # print()
-        best_motif += char
+    print(f"Profile Matrix Dimension: ({profile_matrix.shape[0]}, {profile_matrix.shape[1]})")
+    for pos in range(profile_matrix.shape[1]):
+        seq = ""
+        score = -1
+        for char in range(profile_matrix.shape[0]):
+            if profile_matrix[char][pos] > score:
+                score = profile_matrix[char][pos]
+                if char == 0:
+                    seq = 'A'
+                elif char == 1:
+                    seq = 'C'
+                elif char == 2:
+                    seq = 'G'
+                elif char == 3:
+                    seq = 'T'
+        best_motif += seq
         best_score += score
-                
-    # print(profile_matrix)
-    return best_motif, score
+    return best_motif, best_score
 
 
-# Function to generate all possible motifs of length l
+def generate_profile_matrix(motifs):
+    T = len(motifs)
+    L_mer = len(motifs[0])
+    profile_matrix = np.zeros((4, L_mer))   # 4*L matrix
+    for i in range(L_mer):
+        for j in range(T):
+            if motifs[j][i].lower() == 'a':
+                profile_matrix[0][i] += 1
+            elif motifs[j][i].lower() == 'c':
+                profile_matrix[1][i] += 1
+            elif motifs[j][i].lower() == 'g':
+                profile_matrix[2][i] += 1
+            elif motifs[j][i].lower() == 't':
+                profile_matrix[3][i] += 1
+    return profile_matrix
+
+
 def generate_motifs(L, sequences):
-    ''' Returns a dictonary of motifs for each sequence. '''
-    motifs = {}
+    # motifs is a list of all possible motifs
+    motifs = []
     for seq in sequences:
-        motif_arr = []
         for i in range(len(seq) - L + 1):
-            motif = seq[i:i+L]
-            motif_arr.append(motif)
-        motifs[seq] = motif_arr
+            motifs.append(seq[i:i+L])
+    
     return motifs
 
 
 # Main function to find the best motif
 def find_best_motif(sequences, l_mer):
     motifs = generate_motifs(l_mer, sequences)
-    print("Total Number Of Motifs:", len(motifs)*len(motifs[sequences[0]])) # It should be (N-L+1)*T => (40-6+1)*26 = 35*26 = 910.
-    best_results = {}
-    # motifs is a dictionary {seq: [motif1, motif2, ...]}
-    for seq, motif in motifs.items():
-        best_results[seq] = get_best_motif(motif)
-        # break
-    return best_results
+    print("Total Number Of Motifs:", len(motifs)) # It should be (N-L+1)*T => (40-6+1)*26 = 35*26 = 910.
+    
+    profile_matrix = generate_profile_matrix(motifs)
+    print(profile_matrix)
+    best_motif, best_score = get_best_motif(profile_matrix)
+
+    return best_motif, best_score
 
 
-start = time.time()
-data = pd.read_csv('dna.csv', header=None)
 
-# Convert the DNA sequences to a list.
-sequences = data.values.flatten().tolist()
+if __name__ == "__main__":
+    start = time.time()
+    data = pd.read_csv('Assignment 2/dna.csv', header=None)
 
-best_results = find_best_motif(sequences, 6)
-end = time.time()
+    # Convert the DNA sequences to a list.
+    sequences = data.values.flatten().tolist()
+    best_results = find_best_motif(sequences, 6)
+    end = time.time()
 
-for seq, result in best_results.items():
-    print("Best Motif for", seq, ":", result[0], "Score:", result[1])
+    print("Best Motif:", best_results[0])
+    print("Best Score:", best_results[1])
 
-print("Time taken:", end - start, "seconds")
+    print("Time taken:", end - start, "seconds")
